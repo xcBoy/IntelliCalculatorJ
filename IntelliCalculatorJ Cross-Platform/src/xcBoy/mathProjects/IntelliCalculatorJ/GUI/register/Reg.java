e03588c00ff1fed09f66753c29d60f9e83739cff
xcBoy <czeng99@live.com>
2013-07-02 22:00:38 +0800

    完善部分.

---
+++ IntelliCalculatorJ Cross-Platform/src/xcBoy/mathProjects/IntelliCalculatorJ/GUI/register/Reg.java
@@ -0,0 +1,20 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package xcBoy.mathProjects.IntelliCalculatorJ.GUI.register;
+
+/**
+ *
+ * @author zengchen
+ */
+public class Reg {
+    int codeFromString = 0;
+    private Reg(){}
+    public Reg (String regCode) {
+       int hashCode = regCode.hashCode();
+    }
+    private boolean isOdd (int code) {
+        return false;
+    }
+}
